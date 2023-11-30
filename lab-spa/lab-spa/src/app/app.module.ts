cc911c834f47a717a43e844394b14f6936169b79
LucaFelipe <luca.felipe@ges.inatel.br>
2023-11-30 03:28:19 -0300

    Formulário com Angular

--- lab-spa/lab-spa/src/app/app.module.ts
+++ lab-spa/lab-spa/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutoTabelaComponent } from './components/produto-tabela/produto-tabela.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoTabelaComponent,
+    NotFoundComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ lab-spa/lab-spa/src/app/components/produto-tabela/produto-tabela.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-tabela',
+  templateUrl: './produto-tabela.component.html',
+  styleUrls: ['./produto-tabela.component.scss']
+})
+export class ProdutoTabelaComponent {
+
+}
